c3016eef88891f5ab30967598d80fcf72c3d4161
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2023-10-27 17:14:25 -0300

    Desafio de projeto - Escrevendo as Classes de Um Jogo

---
+++ 7 - Estrutura de Dados e Objetos/7.4 Desafio do Projeto - Escrevendo as Classes de um Jogo/index.js
@@ -0,0 +1,34 @@
+// Defina a classe Heroi
+class Heroi {
+  constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+  }
+
+  atacar() {
+      let ataque;
+      switch (this.tipo) {
+          case 'mago':
+              ataque = 'usou magia';
+              break;
+          case 'guerreiro':
+              ataque = 'usou espada';
+              break;
+          case 'monge':
+              ataque = 'usou artes marciais';
+              break;
+          case 'ninja':
+              ataque = 'usou shuriken';
+              break;
+          default:
+              ataque = 'usou um ataque indefinido';
+      }
+
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Teste da classe Heroi
+const meuHeroi = new Heroi('Super Herói', 30, 'mago');
+meuHeroi.atacar();
